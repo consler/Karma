3e3365d98595c083d6ba70bab91fe3765223479f
consler <consler@localhost.localdomain>
2025-03-01 22:50:59 +0100

    health boost effect added

--- src/main/java/my/consler/karma/karma/Karma/Effects.java
+++ src/main/java/my/consler/karma/karma/Karma/Effects.java
@@ -9,37 +9,71 @@ public class Effects
 
     public static void update(Player player, int karma)
     {
-        AttributeInstance attackDamage = player.getAttribute(Attribute.ATTACK_DAMAGE);
-        assert attackDamage != null;
+        AttributeInstance attack_damage = player.getAttribute(Attribute.ATTACK_DAMAGE);
+        assert attack_damage != null;
+        AttributeInstance health = player.getAttribute(Attribute.MAX_HEALTH);
+        assert health != null;
+        AttributeInstance mining_speed = player.getAttribute(Attribute.BLOCK_BREAK_SPEED);
+        assert  mining_speed != null;
 
-        if (karma > 2500)
+        if (karma > 2000)
         {
-            attackDamage.setBaseValue(7);
+            attack_damage.setBaseValue(4);
+            health.setBaseValue(28);
+
+        }
+        else if (karma > 1500)
+        {
+            attack_damage.setBaseValue(3.5);
+            health.setBaseValue(26);
 
         }
         else if (karma > 1000)
         {
-            attackDamage.setBaseValue(4);
+            attack_damage.setBaseValue(3);
+            health.setBaseValue(24);
+
+        }
+        else if (karma > 600)
+        {
+            attack_damage.setBaseValue(2);
+            health.setBaseValue(22);
 
         }
         else if (karma > 200)
         {
-            attackDamage.setBaseValue(2);
+            attack_damage.setBaseValue(1.5);
+            health.setBaseValue(22);
 
         }
-        else if (karma < -2500)
+        else if (karma < -2000)
         {
-            attackDamage.setBaseValue(-6);
+            attack_damage.setBaseValue(-5);
+            health.setBaseValue(12);
+
+        }
+        else if (karma < -1500)
+        {
+            attack_damage.setBaseValue(-4);
+            health.setBaseValue(14);
 
         }
         else if (karma < -1000)
         {
-            attackDamage.setBaseValue(-3);
+            attack_damage.setBaseValue(-3);
+            health.setBaseValue(16);
+
+        }
+        else if (karma < -600)
+        {
+            attack_damage.setBaseValue(-2);
+            health.setBaseValue(18);
 
         }
         else if (karma < -200)
         {
-            attackDamage.setBaseValue(1);
+            attack_damage.setBaseValue(1);
+            health.setBaseValue(20);
 
         }
 
