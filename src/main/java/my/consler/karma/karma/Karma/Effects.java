c558c993359e7a95c44be0ef0f9fc976d012cf8f
consler <consler@gmail.com>
2025-03-01 23:07:27 +0100

    added mining speed as en effect

--- src/main/java/my/consler/karma/karma/Karma/Effects.java
+++ src/main/java/my/consler/karma/karma/Karma/Effects.java
@@ -20,60 +20,70 @@ public class Effects
         {
             attack_damage.setBaseValue(4);
             health.setBaseValue(28);
+            mining_speed.setBaseValue(2);
 
         }
         else if (karma > 1500)
         {
             attack_damage.setBaseValue(3.5);
             health.setBaseValue(26);
+            mining_speed.setBaseValue(1.8);
 
         }
         else if (karma > 1000)
         {
             attack_damage.setBaseValue(3);
             health.setBaseValue(24);
+            mining_speed.setBaseValue(1.6);
 
         }
         else if (karma > 600)
         {
             attack_damage.setBaseValue(2);
             health.setBaseValue(22);
+            mining_speed.setBaseValue(1.4);
 
         }
         else if (karma > 200)
         {
             attack_damage.setBaseValue(1.5);
             health.setBaseValue(22);
+            mining_speed.setBaseValue(1.2);
 
         }
         else if (karma < -2000)
         {
             attack_damage.setBaseValue(-5);
             health.setBaseValue(12);
+            mining_speed.setBaseValue(0.3);
 
         }
         else if (karma < -1500)
         {
             attack_damage.setBaseValue(-4);
             health.setBaseValue(14);
+            mining_speed.setBaseValue(0.6);
 
         }
         else if (karma < -1000)
         {
             attack_damage.setBaseValue(-3);
             health.setBaseValue(16);
+            mining_speed.setBaseValue(0.8);
 
         }
         else if (karma < -600)
         {
             attack_damage.setBaseValue(-2);
             health.setBaseValue(18);
+            mining_speed.setBaseValue(0.9);
 
         }
         else if (karma < -200)
         {
             attack_damage.setBaseValue(1);
             health.setBaseValue(20);
+            mining_speed.setBaseValue(1);
 
         }
 
